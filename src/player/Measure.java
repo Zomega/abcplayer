da3e6e6c874f1b211db653c20230a4e1fe3562c3
Will Oursler <woursler@hephaestus.(none)>
2013-03-17 17:29:32 -0400

    Makes a quick Pair<First,Second> class...

--- src/player/Measure.java
+++ src/player/Measure.java
@@ -1,5 +1,15 @@
 package player;
 
+import java.util.List;
+
+import utilities.Fraction;
+import utilities.Pair;
+
 public class Measure {
+	
+	// Member variables.
+	Measure next;
+	Measure alternateNext = null;
+	List<Pair<Note,Fraction>> notes;
 
 }
