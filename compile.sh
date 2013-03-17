ba68824ffee340bff23bac7b6470272c7e3ecc58
Will Oursler <woursler@hephaestus.(none)>
2013-03-17 16:44:46 -0400

    New figures and additions to the doc...

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
-# TODO: make more general. Compile all .dot to .svg...
-dot -T svg design/woursler/classes.dot > design/woursler/classes.svg
+# TODO: make more general. Compile all .dot to .svg and .png...
+dot -T svg design/classes.dot > design/classes.svg
+dot -T svg design/measure_example.dot > design/measure_example.svg
+dot -T png design/classes.dot > design/classes.png
+dot -T png design/measure_example.dot > design/measure_example.png
 # TODO: compile all .tex to .pdf
