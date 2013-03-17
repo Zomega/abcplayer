55409fbfeeb7538dff34f23079c2b5c34bf2f678
Will Oursler <woursler@hephaestus.(none)>
2013-03-17 16:57:16 -0400

    Adds new classes to dep graph, minor commits.

--- compile.sh
+++ compile.sh
@@ -3,4 +3,10 @@ dot -T svg design/classes.dot > design/classes.svg
 dot -T svg design/measure_example.dot > design/measure_example.svg
 dot -T png design/classes.dot > design/classes.png
 dot -T png design/measure_example.dot > design/measure_example.png
+
 # TODO: compile all .tex to .pdf
+cd design
+pdflatex design_notes.tex
+cd ../docs
+pdflatex team-contract.tex
+cd ..
