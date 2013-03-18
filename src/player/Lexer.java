beb1addf5be824ea5cc86ab4fa08d25c866699e1
Will Oursler <woursler@hephaestus.(none)>
2013-03-17 22:45:05 -0400

    Imports woursler's ps2 Token / Parser class.

--- src/player/Lexer.java
+++ src/player/Lexer.java
@@ -1,5 +1,64 @@
 package player;
 
-public class Lexer {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.Scanner;
 
+/**
+ * A lexer takes a string and splits it into Tokens that are meaningful to a
+ * parser.
+ * 
+ * Valid tokens are defined by the Token Class.
+ * 
+ * At the time of writing, this is woursler's PS 2 token class, unchanged...
+ * 
+ * @author woursler
+ * 
+ */
+public class Lexer {
+	/**
+	 * Lexes the passed string into Tokens.
+	 * 
+	 * @param input
+	 *            a string to be split into Tokens.
+	 * @return a List of Tokens.
+	 * @throws RuntimeException
+	 *             if something that does appear to be a valid token is
+	 *             encountered.
+	 */
+	public static List<Token> lex(String input) throws RuntimeException {
+		List<Token> tokens = new ArrayList<Token>();
+		Scanner scanner = new Scanner(input);
+		scanner.useDelimiter("\\p{javaWhitespace}+");
+
+		// TODO: Remove Scanner ( which has some lexing functions, even if I
+		// don't use them ) and recognize and strip whitespace automagically?
+
+		while (scanner.hasNext()) {
+			String current = scanner.next();
+
+			while (current.length() > 0) {
+
+				boolean foundValidToken = false;
+
+				for (Token.Type type : Token.Type.values()) {
+					Matcher matcher = Token.getPattern(type).matcher(current);
+					if (matcher.lookingAt()) {
+						tokens.add(new Token(current.substring(matcher.start(),
+								matcher.end()), type));
+						current = current.substring(matcher.end());
+						foundValidToken = true;
+						break;
+					}
+				}
+
+				if (!foundValidToken) {
+					throw new RuntimeException("Invalid token in \"" + current
+							+ "\".");
+				}
+			}
+		}
+		return tokens;
+	}
 }
--- src/player/Token.java
+++ src/player/Token.java
@@ -1,5 +1,81 @@
 package player;
 
-public class Token {
+import java.util.regex.Pattern;
 
+/**
+ * A token is a lexical item that the parser uses.
+ * 
+ * At the time of writing, this is woursler's PS 2 token class, unchanged...
+ * 
+ * @author woursler
+ * 
+ */
+public class Token {
+	/**
+	 * All the types of valid tokens.
+	 */
+	public static enum Type {
+		VARIABLE, INTEGER, FLOATING_POINT, OPERATOR, OPEN_PAREN, CLOSE_PAREN
+	}
+
+	/**
+	 * Method to access a definition for each possible token type.
+	 * 
+	 * @param type
+	 *            a Token.Type to look up.
+	 * @return a Pattern containing a regex that will match any valid token of
+	 *         this type. Will return null if an invalid argument is given.
+	 */
+	public static Pattern getPattern(Token.Type type) {
+		switch (type) {
+		case INTEGER:
+			return Pattern.compile("\\d+(?!\\.)");
+		case FLOATING_POINT:
+			return Pattern.compile("(\\d*\\.\\d+(?!\\.))|(\\d+\\.\\d*(?!\\.))");
+		case VARIABLE:
+			return Pattern.compile("[a-zA-Z]+");
+		case OPERATOR:
+			return Pattern.compile("[*+]");
+		case OPEN_PAREN:
+			return Pattern.compile("\\(");
+		case CLOSE_PAREN:
+			return Pattern.compile("\\)");
+		default:
+			return null;
+		}
+	}
+
+	private final Type type;
+	private final String contents;
+
+	/**
+	 * Basic constructor for a Token instance.
+	 * 
+	 * @param contents
+	 *            the String that makes up the token.
+	 * @param type
+	 *            the Token.Type that this token is.
+	 */
+	public Token(String contents, Token.Type type) {
+		this.type = type;
+		this.contents = contents;
+	}
+
+	/**
+	 * @return the Token.Type of this token.
+	 */
+	public Type getType() {
+		return type;
+	}
+
+	/**
+	 * @return the contents of this token.
+	 */
+	public String getContents() {
+		return contents;
+	}
+
+	public String toString() {
+		return contents;
+	}
 }
