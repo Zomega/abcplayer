094c584483bb92a11faf90f0c85f775b17307460
Will Oursler <woursler@hephaestus.(none)>
2013-03-18 00:08:26 -0400

    Final work commit. Updates some docs / small semantic changes.

--- src/lexer/TokenType.java
+++ src/lexer/TokenType.java
@@ -11,11 +11,11 @@ import java.util.regex.Pattern;
  * 
  */
 public class TokenType {
-	public final String id;
+	public final String name;
 	public final Pattern pattern;
 
-	public TokenType(String id, Pattern pattern) {
-		this.id = id;
+	public TokenType(String name, Pattern pattern) {
+		this.name = name;
 		this.pattern = pattern;
 	}
 }
