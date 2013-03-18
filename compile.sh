7327cbea7b591fdb2fdd18069f04b3c9b4615b23
Will Oursler <woursler@hephaestus.(none)>
2013-03-18 13:34:49 -0400

    Clean up docs / update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,12 +1,11 @@
 # TODO: make more general. Compile all .dot to .svg and .png...
-dot -T svg design/classes.dot > design/classes.svg
-dot -T svg design/measure_example.dot > design/measure_example.svg
-dot -T png design/classes.dot > design/classes.png
-dot -T png design/measure_example.dot > design/measure_example.png
+dot -T svg docs/classes.dot > docs/classes.svg
+dot -T svg docs/measure_example.dot > docs/measure_example.svg
+dot -T png docs/classes.dot > docs/classes.png
+dot -T png docs/measure_example.dot > docs/measure_example.png
 
 # TODO: compile all .tex to .pdf
-cd design
-pdflatex design_notes.tex
-cd ../docs
+cd docs
+pdflatex design-milestone.tex
 pdflatex team-contract.tex
 cd ..
